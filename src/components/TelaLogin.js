6dbe8d688f71ea14af8b1a98ae1078bde10adb8f
kxvinn <kevin13.dourado@gmail.com>
2023-08-28 10:16:50 -0300

    branch com o código da aula do dia Mon, Aug 28, 2023 10:16:50 AM

---
+++ src/components/TelaLogin.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+const PercentageDimensionsBasics = () => {
+return (
+    <View style={{height:'100%'}}>
+    <View
+        style={{
+            height: '15%',
+            backgroundColor: 'powderblue',
+        }}
+        />
+
+            <Text style={styles.heading}>Tela de Login</Text>
+            <TextInput
+            style = {styles.input}
+            placeholder="senha"
+            />
+  </View>    
+    );
+    };       
+    const styles = StyleSheet.create({
+        heading: {
+            fontSize: 20,
+            fontWeight: 'bold',
+            marginBottom: 20,
+        },
+        input:{
+            width:'80%',
+            padding: 10,
+            marginBottom: 10,
+            borderwidth: 1,
+            borderColor: 'gray',
+
+        },
+    }); 
+    export default PercentageDimensionsBasics;
